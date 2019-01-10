c8b33eee86d97a992d82cd4388fe0bb0c4595455
Yohei Igata <yohei.igata@gmail.com>
2019-01-10 17:08:28 +0900

    Add module uart DTR line

--- cores/Arduino/Boards/STM32F439VITx/Inc/main.h
+++ cores/Arduino/Boards/STM32F439VITx/Inc/main.h
@@ -78,6 +78,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define DTR_MODULE_Pin GPIO_PIN_5
+#define DTR_MODULE_GPIO_Port GPIOC
 /* USER CODE BEGIN Private defines */
 
 /* USER CODE END Private defines */
